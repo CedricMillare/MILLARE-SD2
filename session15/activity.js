19caac063ec9885823dd44b71256256b465f2999
CedricMillare <cmamillare.student@ua.edu.ph>
2026-03-12 19:51:48 +0800

    Added session15 and miniActivities

---
+++ session15/activity.js
@@ -0,0 +1,18 @@
+// Mini-activity1
+// Using prompt convert Celsius to Kelvin
+// Present the output using alert
+
+let celsius = parseInt(prompt("Enter the temperature in Celsius: "));
+let kelvin = 273.15;
+alert("Temperature in Kelvin: " + (celsius + kelvin));
+
+
+
+
+// Mini-activity2
+// Using prompt get circle area
+// Present the output using alert
+
+let radius = parseInt(prompt("Enter the radius of the circle: "));
+let area = 3.14 * radius * radius;
+alert("Area of the circle: " + area);
